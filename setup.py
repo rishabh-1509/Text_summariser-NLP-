61bf04ad1baa9deeb9077acac09507f74c4f3bac
rishabh-1509 <rishabh09dwivedi@gmail.com>
2024-03-06 12:54:48 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()\
+    
+__version__ = "0.0.0"
+
+Repo_name = 'Text_summariser-NLP-'
+author_name = 'rishabh-1509'
+author_Email = 'rishabh-1509@gmail.com'
+SRC_REPO = "text_summariser"
+
+setuptools.setup(
+    name= SRC_REPO,
+    version = __version__,
+    author=author_name,
+    author_email=author_Email,
+    description="Text Summarizer - a simple text summarizer nlp project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rishabh-1509/Text_summariser-NLP",
+    project_urls={
+        "Bug Tracker": "https://github.com/rishabh-1509/Text_summariser-NLP/issues",
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages("src"),
+)
