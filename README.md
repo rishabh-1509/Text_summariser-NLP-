20c087eb7d58ce972371539d10da332a63414eee
rishabh-1509 <rishabh09dwivedi@gmail.com>
2024-03-07 16:25:40 +0530

    further maintains

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Text_summariser-NLP-
 This project is NLP end to end project that help in text summarization.... 
+## Workflow
+
+1. Update config.yaml file 
+2. update params.yaml 
+3. update entity 
+4. update the configuration manager in src config
+5. update the components 
+6. update the pipeline  
+7. update the main.py 
+8. update the app.py 
--- src/text_summarizer/utlis/common.py
+++ src/text_summarizer/utlis/common.py
@@ -20,30 +20,30 @@ def read_yaml (path_to_yaml: Path) -> ConfigBox:
     Returns: I
         ConfigBox: ConfigBox type
     """
-try:
-    with open (path_to_yaml) as yaml_file:
-        content = yaml.safe_load (yaml_file)
-        logger.info(f"yaml file: {path_to_yaml} loaded successfully")
-        return ConfigBox(content)
+    try:
+                    with open (path_to_yaml) as yaml_file:
+                        content = yaml.safe_load (yaml_file)
+                        logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+                        return ConfigBox(content)
 
-except BoxValueError:
-    raise ValueError("yaml file is empty")
+    except BoxValueError:
+                    raise ValueError("yaml file is empty")
 
-except Exception as e:
-    raise e
+    except Exception as e:
+                     raise e
 
 
 @ensure_annotations
 def create_directories (path_to_directories: list, verbose = True):
-    """create list of directories
-    Args:
-        path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs
-    """
-for path in path_to_directories:
-    os.makedirs (path, exist_ok=True)
-    if verbose:
-        logger.info(f"created directory at: {path}")
+#    """create list of directories
+#     Args:
+#         path_to_directories (list): list of path of directories
+#         ignore_log (bool, optional): ignore if multiple dirs
+#     """
+        for path in path_to_directories:
+                    os.makedirs (path, exist_ok=True)
+                    if verbose:
+                        logger.info(f"created directory at: {path}")
 
 
 @ensure_annotations
