874cce5cb7842cb7105bb500726fa992cb1f8de4
rishabh-1509 <rishabh09dwivedi@gmail.com>
2024-03-18 21:18:40 +0530

    solved bg

--- src/text_summarizer/entity/__init__.py
+++ src/text_summarizer/entity/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
-    root_dir = Path
-    source_url = str
-    local_data_file = Path
-    unzip_dir = Path
+    root_dir : Path
+    source_url : str
+    local_data_file : Path
+    unzip_dir : Path
